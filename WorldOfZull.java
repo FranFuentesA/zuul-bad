79725ea3ccfa0aa5dd544c567826a8fb74af0452
FranFuentesA <fran_fuentes20@hotmail.com>
2016-05-19 10:35:30 +0200

    0127 Añadida clase main y creacion de archivo ejecutable

---
+++ WorldOfZull.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class WorldOfZull here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZull
+{
+    // instance variables - replace the example below with your own
+    private static Game game;
+
+    /**
+     * Constructor for objects of class WorldOfZull
+     */
+    public static void main (String [ ] args)
+    {
+       game = new Game();
+       game.play();
+    }
+    
+}
